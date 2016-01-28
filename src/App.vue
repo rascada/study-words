a62018ad37cf9f9d0bac3d8bf1c03d6ce6347c44
rascada <carftjr@gmail.com>
2016-01-28 23:47:15 +0100

    split words to modules

--- src/App.vue
+++ src/App.vue
@@ -11,89 +11,14 @@
 
 <script>
 
+import words from './words/second';
+
 export default {
   ready() {
-    var words = [
-    	['piec, wypalać', 'backen', 'buk', 'hat gebacken'],
-    	['zaczynać', 'beginnen', 'buk', 'hat gebacken'],
-    	['uchronić, kryć', 'bergen', 'barg', 'hat geborgen'],
-    	['oferować', 'bieten', 'bot', 'hat geboten'],
-    	['wiązać, łączyć', 'binden', 'band', 'hat gebunden'],
-    	['prosić', 'bitten', 'bat', 'hat gebeten'],
-    	['zostawać', 'bleiben', 'blieb', 'ist geblieben'],
-    	['piec, smażyć', 'braten', 'briet', 'hat gebraten'],
-    	['łamać, kruszyć', 'brechen', 'brach', 'hat gebrochen'],
-    	['palić się, płonąć', 'brennen', 'brannte', 'hat gebrannt'],
-    	['przynosić', 'bringen', 'brachte', 'hat gebracht'],
-    	['myśleć', 'denken', 'dachte', 'hat gedacht'],
-    	['móc (mieć pozwolenie)', 'dürfen', 'durfte', 'hat gedurft'],
-    	['polecać, rekomendować', 'empfehlen', 'empfahl', 'hat empfohlen'],
-    	['decydować', 'entscheiden', 'entschied', 'hat entschieden'],
-    	['jeść', 'essen', 'aß', 'hat gegessen'],
-    	['jechać, zawieźć', 'fahren', 'fuhr', 'ist/hat gefahren'],
-    	['upadać, spadać', 'fallen', 'fiel', 'ist gefallen'],
-    	['łapać', 'fangen', 'fing', 'hat gefangen'],
-    	['znajdować', 'finden', 'fand', 'hat gefunden'],
-    	['latać', 'fliegen', 'flog', 'ist geflogen'],
-    	['płynąć, cieknąć', 'fließen', 'floß', 'ist geflossen'],
-    	['rodzić', 'gebären', 'gebar', 'hat geboren'],
-    	['dawać', 'geben', 'gab', 'hat gegeben'],
-    	['chodzić', 'gehen', 'ging', 'ist gegangen'],
-    	['udawać się, powieść się', 'gelingen', 'gelang', 'ist gelungen'],
-    	['spożywać', 'genießen', 'genoß', 'hat genossen'],
-    	['stać się dziać się', 'geschehen', 'geschah', 'ist geschehen'],
-    	['wygrywać', 'gewinnen', 'gewann', 'hat gewonnen'],
-    	['nalewać, lać', 'gießen', 'goß', 'hat gegossen'],
-    	['wyrównywać', 'gleichen', 'glich', 'hat geglichen'],
-    	['ślizgać się, szybować', 'gleiten', 'glitt', 'ist geglitten'],
-    	['łapać, ujmować', 'greifen', 'griff', 'hat gegriffen'],
-    	['mieć', 'haben', 'hatte', 'hat gehabt'],
-    	['trzymać', 'halten', 'hielt', 'hat gehalten'],
-    	['podnosić', 'heben', 'hob', 'hat gehobem'],
-    	['nazywać się', 'heißen', 'hieß', 'hat geheißen'],
-    	['pomagać', 'helfen', 'half', 'hat geholfen'],
-    	['znać', 'kennen', 'kannte', 'hat gekannt'],
-    	['przychodzić', 'kommen', 'kam', 'ist gekommen'],
-    	['móc, umieć', 'können', 'konnte', 'hat gekonnt'],
-    	['ładować', 'laden', 'lud', 'hat geladen'],
-    	['pozwalać', 'lassen', 'ließ', 'hat gelassen'],
-    	['biegać, pędzić', 'laufen', 'lief', 'ist gelaufen'],
-    	['cierpieć', 'leiden', 'litt', 'hat gelitten'],
-    	['pożyczać', 'leihen', 'lieh', 'hat geliehen'],
-    	['czytać', 'lesen', 'las', 'hat gelesen'],
-    	],
-    	answer =  document.getElementById('answer'),
+    var answer =  document.getElementById('answer'),
     	word = document.getElementById('word'),
     	active = Math.floor(Math.random() * words.length),
     	[round, correctAnsw] = [0, 0];
-
-    /*
-    		['grać', 'spielen', 'spielte', 'gespielt'],
-    		['mieszkać', 'wohnen', 'wohnte', 'gewohnt'],
-    		['malować', 'malen', 'malte', 'gemalt'],
-    		['płakać', 'weinen', 'weinte', 'geweint'],
-    		['pytać', 'fragen', 'fragte', 'gefragt'],
-    		['robić', 'machen', 'machte', 'gemacht'],
-    		['ćwiczyć', 'üben', 'übte', 'geübt'],
-    		['parkować', 'parken', 'parkte', 'geparkt'],
-    		['śmiać się', 'lachen' , 'lachte', 'gelacht'],
-    		['słuchać', 'hören', 'hörte' , 'gehört'],
-    		['fotografować', 'fotografieren', 'fotografierte', 'gefotografiert'],
-    		['zamawiać', 'bestellen', 'bestellte', 'gebestellt'],
-    		['żyć', 'leben', 'lebte', 'gelebt'],
-    		['czyścić', 'putzen', 'putzte', 'geputzt'],
-    		['szukać', 'suchen', 'suchte', 'gesucht'],
-    		['budować', 'bauen', 'baute', 'gebaut'],
-    		['pokazywać', 'zeigen', 'zeigte', 'zeigt'],
-    		['wybierać', 'wählen', 'wählte', 'gewählt'],
-    		['wręczać', 'schenken', 'schenkte', 'geschenkt'],
-    		['kochać', 'lieben', 'liebte', 'geliebt'],
-    		['produkować', 'produzieren', 'produzierte', 'geproduziert'],
-    		['odbierać', 'apholen', 'holte ap', 'apgeholt'],
-    		['obchodzić, świętować', 'feiern', 'feiernte', 'gefeiernt'],
-    		['zarabiać', 'verdienen', 'verdiente', 'geverdient']
-
-    */
 
     words.map((elem) => elem[4] = 0);
 
