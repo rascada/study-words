05d45ec621d7436a392f6b6ca6e5a5febfbc1357
rascada <carftjr@gmail.com>
2016-02-22 21:36:24 +0100

    add ad

---
+++ src/ad.vue
@@ -0,0 +1,26 @@
+<template>
+  <script type='text/javascript'>
+    var googletag = googletag || {};
+    googletag.cmd = googletag.cmd || [];
+    (function() {
+      var gads = document.createElement('script');
+      gads.async = true;
+      gads.type = 'text/javascript';
+      var useSSL = 'https:' == document.location.protocol;
+      gads.src = (useSSL ? 'https:' : 'http:') +
+        '//www.googletagservices.com/tag/js/gpt.js';
+      var node = document.getElementsByTagName('script')[0];
+      node.parentNode.insertBefore(gads, node);
+    })();
+  </script>
+
+  <script type='text/javascript'>
+    googletag.cmd.push(function() {
+      googletag.defineSlot('/21882922/slowka', [1024, 768], 'div-gpt-ad-1456170299072-0').addService(googletag.companionAds()).addService(googletag.pubads());
+      googletag.pubads().enableSingleRequest();
+      googletag.pubads().enableVideoAds();
+      googletag.companionAds().setRefreshUnfilledSlots(true);
+      googletag.enableServices();
+    });
+  </script>
+</template>
