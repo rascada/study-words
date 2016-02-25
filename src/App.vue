9667bddf145ec36c4587aedd62dca9c39ae037f3
rascada <carftjr@gmail.com>
2016-02-25 20:25:18 +0100

    global button style

--- src/App.vue
+++ src/App.vue
@@ -53,6 +53,22 @@ body
   height 100vh
   margin 0
 
+  button
+    outline none
+    font-weight bold
+    background none
+    cursor pointer
+    padding .75em .5em
+    border-radius .1em
+    color #fff
+    border 0
+
+    transition .3s
+    &:hover
+      box-shadow 0 .1em .2em rgba(#333, .5)
+      background #fff
+      color #2da
+
 .studyWords
   height 100%
   flex styl
@@ -72,22 +88,6 @@ body
 
     h1
       cursor pointer
-
-    button
-      outline none
-      font-weight bold
-      background none
-      cursor pointer
-      padding .75em .5em
-      border-radius .1em
-      color #fff
-      border 0
-
-      transition .3s
-      &:hover
-        box-shadow 0 .1em .2em rgba(#333, .5)
-        background #fff
-        color #2da
 
   main
     flex styl
